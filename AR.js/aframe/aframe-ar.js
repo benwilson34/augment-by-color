2bec2c46a4be6602157d5b4709aa713429f547ed
benwilson34 <ben.wilson1337@gmail.com>
2017-03-30 17:04:50 -0400

    Update aframe-ar.js

--- AR.js/aframe/aframe-ar.js
+++ AR.js/aframe/aframe-ar.js
@@ -175,6 +175,9 @@ AFRAME.registerComponent('artoolkitmarker', {
 		}else if( this.data.preset === 'kanji' ){
 			this.data.type = 'pattern'
 			this.data.patternUrl = THREEx.ArToolkitContext.baseURL+'../data/data/patt.kanji'			
+		}else if( this.data.preset === 'poster' ){
+			this.data.type = 'pattern'
+			this.data.patternUrl = THREEx.ArToolkitContext.baseURL+'../data/data/patt.poster1'			
 		}else {
 			console.assert( this.data.preset === '', 'illegal preset value '+this.data.preset)
 		}
